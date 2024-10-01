b08938a30e54902e13d2ffa5c55422ffa42e5a69
Kyle Barrett <kab102395@gmail.com>
2024-09-30 20:51:50 -0700

    firebase commit 2

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
+const {hello} = require("../cd my-napi-project/napi-rs");
+console.log(hello()); // Outputs: Hello from Rust!
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
